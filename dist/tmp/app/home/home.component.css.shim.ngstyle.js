fdd26bb35e02f4a442378fc7df8a5f565760f678
rossedfort <ross.edfort@wazeedigital.com>
2017-01-03 13:42:10 -0700

    ignore files that aren't necessary i.e. /node_modules

--- dist/tmp/app/home/home.component.css.shim.ngstyle.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-exports.styles = ['[_nghost-%COMP%]{display:block;padding:0 16px}input[_ngcontent-%COMP%]{width:250px}ul[_ngcontent-%COMP%]{list-style-type:none;padding:0 0 0 8px}'];
--- dist/tmp/app/operators.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-require('rxjs/add/observable/throw');
-require('rxjs/add/operator/map');
-require('rxjs/add/operator/catch');
