18106a02f46dad72019fb723e3b807c911d040e3
rossedfort <ross.edfort@wazeedigital.com>
2017-01-04 10:29:31 -0700

    INTERNAL - include scss in build assets task

--- tools/tasks/seed/build.assets.prod.ts
+++ tools/tasks/seed/build.assets.prod.ts
@@ -26,8 +26,6 @@ export = () => {
     '!' + join(Config.APP_SRC, '**', '*.ts'),
     '!' + join(Config.APP_SRC, '**', '*.css'),
     '!' + join(Config.APP_SRC, '**', '*.html'),
-    '!' + join(Config.APP_SRC, '**', '*.scss'),
-    '!' + join(Config.APP_SRC, '**', '*.sass'),
     '!' + join(Config.ASSETS_SRC, '**', '*.js')
   ].concat(Config.TEMP_FILES.map((p) => { return '!' + p; })))
     .pipe(onlyDirs(es))
